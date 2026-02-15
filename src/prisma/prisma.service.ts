b3b4a3c25c43818c861ea1cfcb22b4383dcd9129
iliasabai09 <iliasabaimain@gmail.com>
2026-02-15 16:11:37 +0500

    fix bugs

--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -1,5 +1,5 @@
 import { Injectable, OnModuleDestroy, OnModuleInit } from '@nestjs/common';
-import { PrismaClient } from '@prisma/client';
+import { PrismaClient } from '../generated/prisma/client';
 import { PrismaPg } from '@prisma/adapter-pg';
 import { Pool } from 'pg';
 
@@ -10,16 +10,11 @@ export class PrismaService
 {
   constructor() {
     const url = process.env.DATABASE_URL;
-    if (!url) {
-      throw new Error(
-        'DATABASE_URL is not set. Check your .env file in project root.',
-      );
-    }
+    if (!url) throw new Error('DATABASE_URL is not set');
 
-    // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment,@typescript-eslint/no-unsafe-call
     const pool = new Pool({
       connectionString: url,
-      ssl: { rejectUnauthorized: false },
+      ssl: { rejectUnauthorized: false }, // Neon
     });
 
     super({
