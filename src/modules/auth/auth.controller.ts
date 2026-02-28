1445a2353c2f26d95ae18cebe095689882c0ce70
iliasabai09 <iliasabaimain@gmail.com>
2026-02-28 22:11:34 +0500

    feat: remove unused controller

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Get, Post, UseGuards } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { OtpRequestDto } from './dto/otp-request.dto';
 import { OtpVerifyDto } from './dto/otp-verify.dto';
@@ -10,10 +10,6 @@ import {
   VERIFY_OTP_RES,
 } from '@modules/auth/constants/swagger.constants';
 import { OtpVerifyResponseDto } from '@modules/auth/dto/response/otp-verify.response.dto';
-import { Roles } from '@core/decorators/roles.decorator';
-import { ROLE } from '@modules/auth/enums/role.enum';
-import { AuthGuard } from '@core/guards/jwt-auth.guard';
-import { RolesGuard } from '@core/guards/roles.guard';
 
 @Controller('auth')
 export class AuthController {
@@ -35,10 +31,10 @@ export class AuthController {
     return this.auth.verifyOtp(dto);
   }
 
-  @Get('testAuth')
-  @Roles(ROLE.ADMIN)
-  @UseGuards(AuthGuard, RolesGuard)
-  test() {
-    return 'hajsdajklhflghsdajlfhjgldasgfikj';
-  }
+  // @Get('testAuth')
+  // @Roles(ROLE.ADMIN)
+  // @UseGuards(AuthGuard, RolesGuard)
+  // test() {
+  //   return 'hajsdajklhflghsdajlfhjgldasgfikj';
+  // }
 }
